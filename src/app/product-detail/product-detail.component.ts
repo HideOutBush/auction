b49241b42eca40f7063beb406da4a3ffcbd6f7e0
HideOutBush <418778965@qq.com>
2017-09-09 13:08:34 +0800

    add dynamic comment to product-detail

--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {Comment, Product, ProductService} from '../shared/product.service';
 
@@ -10,6 +10,10 @@ import {Comment, Product, ProductService} from '../shared/product.service';
 export class ProductDetailComponent implements OnInit {
   product: Product;
   comments: Comment[];
+  newRating = 5;
+  newComment = '';
+
+  isCommentHidden = true;
 
   constructor(private routeInfo: ActivatedRoute,
                 private productService: ProductService
@@ -21,4 +25,15 @@ export class ProductDetailComponent implements OnInit {
     this.comments = this.productService.getCommentsForProduct(productId);
   }
 
+  addComment() {
+    const comment = new Comment(0, this.product.id, new Date().toDateString(), 'someone', this.newRating, this.newComment);
+    this.comments.unshift(comment);
+
+    const sum = this.comments.reduce((sum, comment) => sum + comment.rating, 0);
+    this.product.rating = sum / this.comments.length;
+
+    this.newComment = null;
+    this.newRating = 5;
+    this.isCommentHidden = true;
+  }
 }
--- src/app/stars/stars.component.ts
+++ src/app/stars/stars.component.ts
@@ -1,24 +1,41 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges} from '@angular/core';
 
 @Component({
   selector: 'app-stars',
   templateUrl: './stars.component.html',
   styleUrls: ['./stars.component.css']
 })
-export class StarsComponent implements OnInit {
+export class StarsComponent implements OnInit, OnChanges {
+
   @Input()
   public rating = 0;
 
   public stars: boolean[];
 
-  constructor() { }
+  @Input()
+  private readonly = true;
+  @Output()
+  ratingChange: EventEmitter<number> = new EventEmitter();
+
+  constructor() {
+  }
 
   ngOnInit() {
+    // this.stars = [false, false, true, true, true];
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
     this.stars = [];
     for (let i = 1; i <= 5; i++) {
       this.stars.push(i > this.rating);
     }
-    // this.stars = [false, false, true, true, true];
   }
 
+  clickStar(index: number) {
+    if (!this.readonly) {
+      this.rating = index + 1;
+      this.ngOnInit();
+      this.ratingChange.emit(this.rating);
+    }
+  }
 }
